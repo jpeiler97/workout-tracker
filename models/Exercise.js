1276cd9be5e3c2ce53a68c71a56291b00c6338f1
JP Eiler <jp.eiler97@gmail.com>
2021-06-02 13:20:49 -0500

    added some routes in server.js

--- models/Exercise.js
+++ models/Exercise.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
+	type: {
+		type: String
+	},
 	name: {
 		type: String,
 		unique: true
 	},
-	type: {
-		type: String
-	},
+
 	weight: { type: Number },
 	sets: { type: Number },
 	reps: { type: Number },
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 
-//Uncomment after adding models
 const db = require('./models');
 
 const app = express();
@@ -16,8 +15,29 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-//Uncomment after adding models models
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
+
+app.get('/workouts', (req, res) => {
+	db.Workout
+		.find({})
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+app.get('/exercises', (req, res) => {
+	db.Exercise
+		.find({})
+		.then((dbExercise) => {
+			res.json(dbExercise);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
