d31c404b4f8dae1ae8ac099802a84d92a410e1d5
JP Eiler <jp.eiler97@gmail.com>
2021-06-06 09:57:41 -0500

    fixed update route

--- routes/api.js
+++ routes/api.js
@@ -24,8 +24,8 @@ router.post('/api/workouts', ({ body }, res) => {
 		});
 });
 
-router.put('/api/workouts/:id', ({params}, res) => {
-	Workout.findByIdAndUpdate(params.id , {new: true})
+router.put('/api/workouts/:id', (req, res) => {
+	Workout.findByIdAndUpdate(req.params.id , {$push: { exercises: req.body }}, {new: true})
 		.then((dbWorkout) => {
 			console.log(dbWorkout)
 			res.json(dbWorkout);
