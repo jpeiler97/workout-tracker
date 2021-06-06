7ff8e9a03552f4fef9a7a0de8c07f6f8d5694ea5
JP Eiler <jp.eiler97@gmail.com>
2021-06-06 09:32:02 -0500

    modified put route for id

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const { db } = require('../models/Workout');
 const Workout = require('../models/Workout');
+
 
 router.get('/api/workouts', (req, res) => {
 	Workout.find()
@@ -22,10 +24,10 @@ router.post('/api/workouts', ({ body }, res) => {
 		});
 });
 
-router.post('/api/workouts/:id', ({ body }, res) => {
-	const workout = new Workout(body);
-	Workout.create(workout)
+router.put('/api/workouts/:id', ({params}, res) => {
+	Workout.findByIdAndUpdate(params.id , {new: true})
 		.then((dbWorkout) => {
+			console.log(dbWorkout)
 			res.json(dbWorkout);
 		})
 		.catch((err) => {
