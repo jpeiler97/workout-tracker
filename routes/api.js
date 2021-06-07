f004f52c25466b40abc4514b0d0023c7fbedb326
JP Eiler <jp.eiler97@gmail.com>
2021-06-07 10:34:04 -0500

    cleaned up api.js

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,10 @@ const Workout = require('../models/Workout');
 router.get('/api/workouts', (req, res) => {
 	Workout.aggregate([ 
 		{$addFields: {
-			totalDuration: {$sum: '$exercises.duration'}
+			totalDuration: 
+				{
+					$sum: '$exercises.duration'
+				}
 		}}
 	])
 		.then((dbWorkout) => {
@@ -27,31 +30,6 @@ router.post('/api/workouts', ({ body }, res) => {
 		});
 });
 
-router.put('/api/workouts/:id', (req, res) => {
-	Workout.findByIdAndUpdate(req.params.id , {$push: { exercises: req.body }}, {new: true})
-		.then((dbWorkout) => {
-			console.log(dbWorkout)
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
-});
-
-router.get('/api/workouts/range', (req, res) => {
-	Workout.aggregate([ 
-		{$addFields: {
-			totalDuration: {$sum: '$exercises.duration'}
-		}}
-	]).sort({_id: -1}).limit(7)
-		.then((dbWorkout) => {
-			res.json(dbWorkout);
-		})
-		.catch((err) => {
-			res.json(err);
-		});
-});
-
 router.delete('/api/workouts/:id', (req, res) => {
 	Workout.findByIdAndRemove(req.params.id)
 		.then((dbWorkout) => {
@@ -62,5 +40,36 @@ router.delete('/api/workouts/:id', (req, res) => {
 			res.json(err);
 		});
 });
+
+
+router.put('/api/workouts/:id', (req, res) => {
+	Workout.findByIdAndUpdate(req.params.id , {$push: { exercises: req.body }}, {new: true})
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.get('/api/workouts/range', (req, res) => {
+	Workout.aggregate([ 
+		{$addFields: {
+			totalDuration: 
+			{
+				$sum: '$exercises.duration'
+			}
+		}}
+	])
+	.sort({_id: -1})
+	.limit(7)
+		.then((dbWorkout) => {
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -17,9 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { use
 app.use(require('./routes/api.js'));
 app.use(require('./routes/index.js'));
 
-app.get('/exercise', (req, res) => res.sendFile(path.join(__dirname, 'public/exercise.html')));
-app.get('/stats', (req, res) => res.sendFile(path.join(__dirname, 'public/stats.html')));
-
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 });
