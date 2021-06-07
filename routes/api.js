ebd0fbe270db5b3ed3f7547b17a46289fcebfca5
JP Eiler <jp.eiler97@gmail.com>
2021-06-07 09:50:14 -0500

    added get route for dashboard

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { db } = require('../models/Workout');
 const Workout = require('../models/Workout');
 
 
@@ -32,6 +31,20 @@ router.put('/api/workouts/:id', (req, res) => {
 	Workout.findByIdAndUpdate(req.params.id , {$push: { exercises: req.body }}, {new: true})
 		.then((dbWorkout) => {
 			console.log(dbWorkout)
+			res.json(dbWorkout);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.get('/api/workouts/range', (req, res) => {
+	Workout.aggregate([ 
+		{$addFields: {
+			totalDuration: {$sum: '$exercises.duration'}
+		}}
+	]).sort({_id: -1}).limit(7)
+		.then((dbWorkout) => {
 			res.json(dbWorkout);
 		})
 		.catch((err) => {
